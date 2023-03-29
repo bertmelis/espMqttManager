eb76cb3322c070c16e621be3d1e809e4210d74e6
Bert Melis <bertmelis@users.noreply.github.com>
2023-03-29 09:21:46 +0200

    separate Config

---
+++ src/Config.cpp
@@ -0,0 +1,59 @@
+/*
+Copyright (c) 2023 Bert Melis. All rights reserved.
+
+This work is licensed under the terms of the MIT license.  
+For a copy, see <https://opensource.org/licenses/MIT> or
+the LICENSE file.
+*/
+
+#include "Config.h"
+
+#if defined(ARDUINO_ARCH_ESP8266)
+#define EMM_FILESYSTEM LittleFS
+#elif defined(ARDUINO_ARCH_ESP32)
+#define EMM_FILESYSTEM SPIFFS
+#endif
+
+namespace espMqttManagerHelpers {
+
+bool Config::getConfig() {
+  if (!EMM_FILESYSTEM.begin()) {
+     emm_log_e("Error mounting filesystem");
+     return false;
+  }
+  File file = EMM_FILESYSTEM.open(EMM_CONFIG_FILE, EMM_FILE_READ);
+  if (!file) {
+    emm_log_e("Error opening settings.json");
+    return false;
+  }
+  StaticJsonDocument<256> doc;
+  DeserializationError error = deserializeJson(doc, file);
+  if (error) {
+    emm_log_e("JSON deserialization error: %s", error.c_str());
+    return false;
+  }
+  strlcpy(SSID,
+          doc["SSID"] | "",
+          sizeof(SSID));
+  strlcpy(PSK,
+          doc["PSK"] | "",
+          sizeof(PSK));
+  JsonArray hostIPArray = doc["IPAddress"];
+  uint8_t IPAddress_0 = hostIPArray[0] | 0;
+  uint8_t IPAddress_1 = hostIPArray[1] | 0;
+  uint8_t IPAddress_2 = hostIPArray[2] | 0;
+  uint8_t IPAddress_3 = hostIPArray[3] | 0;
+  hostIP = IPAddress(IPAddress_0, IPAddress_1, IPAddress_2, IPAddress_3);
+  strlcpy(hostname,
+          doc["hostname"] | "",
+          sizeof(hostname));
+  port = doc["port"] | 1883;
+  strlcpy(devicename,
+          doc["devicename"] | "",
+          sizeof(devicename));
+  file.close();
+  EMM_FILESYSTEM.end();
+  return true;
+}
+
+}  // end namespace espMqttManagerHelpers
---
+++ src/Config.h
@@ -0,0 +1,42 @@
+/*
+Copyright (c) 2023 Bert Melis. All rights reserved.
+
+This work is licensed under the terms of the MIT license.  
+For a copy, see <https://opensource.org/licenses/MIT> or
+the LICENSE file.
+*/
+
+#pragma once
+
+#ifndef EMM_CONFIG_FILE
+#define EMM_CONFIG_FILE "/config.json"
+#endif
+
+#if defined(ARDUINO_ARCH_ESP8266)
+#include <LittleFS.h>
+#define EMM_FILE_READ "r"
+#elif defined(ARDUINO_ARCH_ESP32)
+#include <SPIFFS.h>
+#define EMM_FILE_READ FILE_READ
+#endif
+#include <IPAddress.h>
+
+#include <ArduinoJson.h>
+
+#include "Logging.h"
+
+namespace espMqttManagerHelpers {
+
+class Config {
+ public:
+  char SSID[64];
+  char PSK[64];
+  IPAddress hostIP;
+  char hostname[64];
+  uint16_t port;
+  char devicename[64];
+
+  bool getConfig();
+};
+
+}  // end namespace espMqttManagerHelpers
